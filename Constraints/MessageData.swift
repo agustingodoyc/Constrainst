e36cfce4016bfe1f0f88a9e978a9859ac2161c44
Agustín Godoy <a.godoy@globant.com>
2022-08-18 14:28:20 -0300

    Exercise 3

---
+++ Constraints/MessageData.swift
@@ -0,0 +1,16 @@
+//
+//  MessageData.swift
+//  Constraints
+//
+//  Created by Agustin Godoy Cosser on 16/08/2022.
+//
+
+import Foundation
+
+struct MessageData: Codable {
+    
+    var username: String
+    var message: String
+    var timeMessage: String
+    
+}
--- Constraints/ViewController.swift
+++ Constraints/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITextViewDelegate {
+    
+    // MARK: Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tableViewData: [MessageData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableViewData = DataLoader().loadMessages()
+        
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "TableViewCell")
+        
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (tableViewData[indexPath.row].username != "Me") {
+            guard let recivedCell = tableView.dequeueReusableCell(withIdentifier: "recivedMessage",
+                                                           for: indexPath) as? RecivedMessage else {
+                return UITableViewCell()
+            }
+            recivedCell.createMessage(tableViewData[indexPath.row])
+            return recivedCell
+        } else {
+            guard let sentCell = tableView.dequeueReusableCell(withIdentifier: "sentMessage",
+                                                           for: indexPath) as? SentMessage else {
+                return UITableViewCell()
+            }
+            sentCell.createMessage(tableViewData[indexPath.row])
+            return sentCell
+        }
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableViewData.count
+    }
 }
-
